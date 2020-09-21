cc4ea72cb55ad1e133197e06756e66aa2ac8eb2b
gangolnaik <69681081+gangolnaik@users.noreply.github.com>
2020-09-21 17:28:21 +0530

    Add files via upload

---
+++ rubberBand.js
@@ -0,0 +1,31 @@
+class RubberBand{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA : bodyA,
+            pointB : pointB,
+            stiffness : 0.04,
+            length : 10,
+        }
+	this.bodyA = bodyA;
+        this.pointB = pointB;
+        this.rubber = Constraint.create(options);
+        World.add(world,this.rubber);
+    }
+  fly(){
+	this.rubber.bodyA=null;
+    }
+    display(){
+
+	if(this.rubber.bodyA){
+        push();
+	    var pointA = this.rubber.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        stroke(48,22,8);
+	    fill("black");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        pop();
+	}
+    }
+  
+}
